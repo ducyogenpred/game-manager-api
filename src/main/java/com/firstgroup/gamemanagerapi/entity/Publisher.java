edc6134d7c4b912dd41dad1a04fad0a144a33e4b
ducyogenpred <ducyogenpred@gmail.com>
2025-05-06 01:23:05 +0800

    Fixed foreign key in Publisher and Developer Entities

--- src/main/java/com/firstgroup/gamemanagerapi/entity/Publisher.java
+++ src/main/java/com/firstgroup/gamemanagerapi/entity/Publisher.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.cglib.core.Local;
 
 import java.awt.*;
 import java.time.LocalDateTime;
@@ -44,6 +43,6 @@ public class Publisher {
     @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
 
-    @OneToMany(mappedBy = "publisherID", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "publisherId", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<Game> game = new HashSet<>();
 }
