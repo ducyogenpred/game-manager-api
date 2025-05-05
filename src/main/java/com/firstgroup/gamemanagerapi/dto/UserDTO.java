0d17dfad55c12c8853996aa5fc1a46cb713d5c84
ducyogenpred <ducyogenpred@gmail.com>
2025-05-05 13:42:08 +0800

    Added description attribute Added Genre Added Review

--- src/main/java/com/firstgroup/gamemanagerapi/dto/UserDTO.java
+++ src/main/java/com/firstgroup/gamemanagerapi/dto/UserDTO.java
@@ -22,5 +22,6 @@ public class UserDTO {
     private String email;
     private String phoneNumber;
     private LocalDate birthDate;
+    private String description;
     private Set<UserGame> userGames;
 }
