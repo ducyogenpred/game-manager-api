f4d6a510fe5ddf43365bf0bf08016b2dd77c8a7d
GenericUser51 <icerap525@gmail.com>
2025-05-06 20:23:04 +0800

    added genre, review DTO's

--- src/main/java/com/firstgroup/gamemanagerapi/request/GenreRO.java
+++ src/main/java/com/firstgroup/gamemanagerapi/request/GenreRO.java
@@ -1,11 +1,11 @@
 package com.firstgroup.gamemanagerapi.request;
 
-import com.firstgroup.gamemanagerapi.entity.Genre;
 import jakarta.validation.constraints.NotBlank;
 
-public class GenreRO {
-    @NotBlank Genre genre;
-    @NotBlank  String name;
-    @NotBlank  String description;
+
+public record GenreRO {
+    @NotBlank Genre genre,
+    @NotBlank  String name,
+    @NotBlank  String description,
 
 }
