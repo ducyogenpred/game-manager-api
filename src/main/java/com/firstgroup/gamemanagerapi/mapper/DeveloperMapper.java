354feec2d368491915a31c47aaa9fe8270f62272
GenericUser51 <icerap525@gmail.com>
2025-05-06 20:13:21 +0800

    added genre, review DTO's

--- src/main/java/com/firstgroup/gamemanagerapi/mapper/DeveloperMapper.java
+++ src/main/java/com/firstgroup/gamemanagerapi/mapper/DeveloperMapper.java
@@ -1,8 +1,13 @@
 package com.firstgroup.gamemanagerapi.mapper;
 
+import com.firstgroup.gamemanagerapi.dto.DeveloperDTO;
+import com.firstgroup.gamemanagerapi.entity.Developer;
+import com.firstgroup.gamemanagerapi.request.DeveloperRo;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-class DeveloperMapper {
-
+public interface DeveloperMapper {
+    DeveloperDTO toDto(Developer developer);
+    DeveloperDTO toEntity(Developer developer);
+    DeveloperDTO toRo(Developer developer);
 }
