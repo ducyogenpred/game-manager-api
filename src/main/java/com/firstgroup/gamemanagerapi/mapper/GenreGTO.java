29e93378fca7619054702ef90a1e6707b3cff07f
GenericUser51 <icerap525@gmail.com>
2025-05-06 20:03:28 +0800

    added genre, review DTO's

---
+++ src/main/java/com/firstgroup/gamemanagerapi/mapper/GenreGTO.java
@@ -0,0 +1,19 @@
+package com.firstgroup.gamemanagerapi.mapper;
+
+
+import com.firstgroup.gamemanagerapi.dto.GenreDTO;
+import com.firstgroup.gamemanagerapi.entity.Genre;
+import com.firstgroup.gamemanagerapi.request.GenreRO;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface GenreGTO {
+    GenreDTO toDto(Genre genre);
+
+    Genre toEntity(GenreDTO dto);
+
+    GenreRO toRO(Genre genre);
+
+
+
+}
