53c48b6030c51497b246ec768e5009a382849656
Dray <dray.lao@gmail.com>
2025-05-07 18:45:18 +0800

    debugged DeveloperRO.java

--- src/main/java/com/firstgroup/gamemanagerapi/request/DeveloperRO.java
+++ src/main/java/com/firstgroup/gamemanagerapi/request/DeveloperRO.java
@@ -4,8 +4,6 @@ import jakarta.validation.constraints.NotBlank;
 
 import java.time.LocalDateTime;
 
-import java.time.LocalDate;
-
 public record DeveloperRO (
     @NotBlank String name,
     @NotBlank String email,
