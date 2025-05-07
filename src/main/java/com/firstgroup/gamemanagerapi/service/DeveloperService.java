8d0475156b9906029cf971d04f2b7761b70d8019
lhayann <zano2320801@mkt.ceu.edu.ph>
2025-05-08 00:39:06 +0800

    added DeveloperService

--- src/main/java/com/firstgroup/gamemanagerapi/service/DeveloperService.java
+++ src/main/java/com/firstgroup/gamemanagerapi/service/DeveloperService.java
@@ -5,6 +5,7 @@ import com.firstgroup.gamemanagerapi.entity.Developer;
 import com.firstgroup.gamemanagerapi.mapper.DeveloperMapper;
 import com.firstgroup.gamemanagerapi.repository.DeveloperRepository;
 import com.firstgroup.gamemanagerapi.request.DeveloperRO;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -34,4 +35,59 @@ public class DeveloperService {
                 .map(developerMapper::toDto)
                 .toList();
     }
+
+    public DeveloperDTO updateDeveloper (Long id, DeveloperRO developerRO) {
+        Developer developer = developerRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Developer not found with id: + id"));
+
+        if (!developer.getEmail().equals(developerRO.email()) &&
+                developerRepository.existsByEmail(developerRO.email())) {
+            throw new IllegalArgumentException("Email already exist");
+        }
+
+        Developer dev = developerMapper.toEntity(developerRO);
+        return developerMapper.toDto(developerRepository.save(dev));
+    }
+
+    public DeveloperDTO getDeveloperById(Long id) {
+        Developer developer = developerRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Developer not found with id: " + id));
+        return developerMapper.toDto(developer);
+    }
+
+    public void deleteDeveloper(Long id) {
+        if (!developerRepository.existsById(id)) {
+            throw new EntityNotFoundException("Developer not found with id: " + id);
+        }
+        developerRepository.deleteById(id);
+    }
+
+    public List<DeveloperDTO> searchDevelopersByName(String name) {
+        return developerRepository.findByNameContainingIgnoreCase(name)
+                .stream()
+                .map(developerMapper::toDto)
+                .toList();
+
+    }
+
+    public DeveloperDTO findDeveloperByName(String name) {
+        return developerRepository.findByNameIgnoreCase(name)
+                .map(developerMapper::toDto)
+                .orElse(null);
+    }
+
+    public List<DeveloperDTO> findDevelopersByMinReviewCount(Integer reviewCount) {
+        return developerRepository.findByReviewCountGreaterThanEqual(reviewCount)
+                .stream()
+                .map(developerMapper::toDto)
+                .toList();
+    }
+
+    public DeveloperDTO findTopRatedDeveloper() {
+        return developerRepository.findByRatingAverageDesc()
+                .map(developerMapper::toDto)
+                .orElse(null);
+    }
+
 }
+
