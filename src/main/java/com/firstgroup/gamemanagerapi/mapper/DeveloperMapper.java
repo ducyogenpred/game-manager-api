43f796675bb775d605c34ba077eceb251f033bf9
edeli <ducyogenpred@gmail.com>
2025-05-08 07:47:42 +0800

    Fixed and generalize the mapper layer

--- src/main/java/com/firstgroup/gamemanagerapi/mapper/DeveloperMapper.java
+++ src/main/java/com/firstgroup/gamemanagerapi/mapper/DeveloperMapper.java
@@ -3,12 +3,10 @@ package com.firstgroup.gamemanagerapi.mapper;
 import com.firstgroup.gamemanagerapi.dto.DeveloperDTO;
 import com.firstgroup.gamemanagerapi.entity.Developer;
 import com.firstgroup.gamemanagerapi.request.DeveloperRO;
-import com.firstgroup.gamemanagerapi.request.DeveloperRo;
-import org.springframework.web.bind.annotation.RestController;
+import org.mapstruct.Mapper;
 
-@RestController
+@Mapper(componentModel = "spring")
 public interface DeveloperMapper {
     DeveloperDTO toDto(Developer developer);
-    Developer toEntity(Developer developer);
-    DeveloperRO toRo(Developer developer);
+    Developer toEntity(DeveloperRO developer);
 }
--- src/main/java/com/firstgroup/gamemanagerapi/mapper/ReviewMapper.java
+++ src/main/java/com/firstgroup/gamemanagerapi/mapper/ReviewMapper.java
@@ -1,15 +1,12 @@
 package com.firstgroup.gamemanagerapi.mapper;
 
-
-
 import com.firstgroup.gamemanagerapi.dto.ReviewDTO;
 import com.firstgroup.gamemanagerapi.entity.Review;
 import com.firstgroup.gamemanagerapi.request.ReviewRO;
 import org.mapstruct.Mapper;
 
+@Mapper(componentModel = "spring")
 public interface ReviewMapper {
     ReviewDTO toDto (Review review);
-    Review toEntity (ReviewDTO dto);
-    ReviewRO toRo (Review review);
-
+    Review toEntity (ReviewRO ro);
 }
