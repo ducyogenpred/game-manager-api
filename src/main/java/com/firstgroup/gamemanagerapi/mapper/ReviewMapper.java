7d7cbebd70eb1378afd8e10dc3dec7cd1dc498f4
GenericUser51 <icerap525@gmail.com>
2025-05-07 23:38:19 +0800

    added genre, review DTO's

--- src/main/java/com/firstgroup/gamemanagerapi/mapper/ReviewMapper.java
+++ src/main/java/com/firstgroup/gamemanagerapi/mapper/ReviewMapper.java
@@ -9,7 +9,7 @@ import org.mapstruct.Mapper;
 
 public interface ReviewMapper {
     ReviewDTO toDto (Review review);
-    Review toEntity (ReviewDRO dto);
+    Review toEntity (ReviewDTO dto);
     ReviewRO toRo (Review review);
 
 }
--- src/main/java/com/firstgroup/gamemanagerapi/request/GenreRO.java
+++ src/main/java/com/firstgroup/gamemanagerapi/request/GenreRO.java
@@ -3,9 +3,9 @@ package com.firstgroup.gamemanagerapi.request;
 import jakarta.validation.constraints.NotBlank;
 
 
-public record GenreRO {
+public record GenreRO (
     @NotBlank Genre genre,
     @NotBlank  String name,
-    @NotBlank  String description,
-
-}
+    @NotBlank  String description
+)
+{}
