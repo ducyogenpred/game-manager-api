cc04c06a02332eadc554cb906bcb38e588a4c973
edeli <ducyogenpred@gmail.com>
2025-05-08 07:50:01 +0800

    Cleared UserController.java

--- src/main/java/com/firstgroup/gamemanagerapi/controller/UserController.java
+++ src/main/java/com/firstgroup/gamemanagerapi/controller/UserController.java
@@ -16,32 +16,4 @@ import java.util.List;
 public class UserController {
 
     private final UserService userService;
-
-    @PostMapping
-    public ResponseEntity<UserDTO> createUser(@Valid @RequestBody UserRO ro) {
-        return ResponseEntity.ok(userService.createUser(ro));
-    }
-
-    @GetMapping
-    public ResponseEntity<List<UserDTO>> getAllUsers() {
-        return ResponseEntity.ok(userService.getAllUsers());
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<UserDTO> getUserById(@PathVariable long id) {
-        return ResponseEntity.ok(userService.getUserById(id));
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<UserDTO> updateUser(
-            @PathVariable long id,
-            @Valid @RequestBody UserRO ro) {
-        return ResponseEntity.ok(userService.updateUser(id, ro));
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteUserById(@PathVariable long id) {
-        userService.deleteUserById(id);
-        return ResponseEntity.noContent().build();
-    }
 }
