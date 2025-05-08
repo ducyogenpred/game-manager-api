924760e7a72e592f6271903dced094e6533d2f3d
edeli <ducyogenpred@gmail.com>
2025-05-08 12:02:58 +0800

    Remade UserService.java

--- src/main/java/com/firstgroup/gamemanagerapi/service/UserService.java
+++ src/main/java/com/firstgroup/gamemanagerapi/service/UserService.java
@@ -1,75 +1,79 @@
 package com.firstgroup.gamemanagerapi.service;
 
-import com.firstgroup.gamemanagerapi.dto.UserDTO;
 import com.firstgroup.gamemanagerapi.entity.User;
 import com.firstgroup.gamemanagerapi.mapper.UserMapper;
 import com.firstgroup.gamemanagerapi.repository.UserRepository;
+import com.firstgroup.gamemanagerapi.request.UserPatchRO;
 import com.firstgroup.gamemanagerapi.request.UserRO;
-import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
-@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@RequiredArgsConstructor()
 public class UserService {
 
     private final UserRepository userRepository;
     private final UserMapper userMapper;
 
-    public UserDTO createUser(@Valid UserRO ro) {
+    @Transactional
+    public User createUser(@Valid UserRO ro) {
         if (userRepository.existsByDisplayName(ro.displayName())) {
-            throw new IllegalArgumentException("Username already exists!");
+            throw new IllegalArgumentException("User with this display name already exists.");
         }
 
         if (userRepository.existsByEmail(ro.email())) {
-            throw new IllegalArgumentException("Email already exists!");
+            throw new IllegalArgumentException("User with this email already exists.");
         }
 
-        User user = userMapper.toEntity(ro);
-        user.setPassword(ro.password());
-
-        return userMapper.toDto(userRepository.save(user));
+        return userRepository.save(userMapper.toEntity(ro));
     }
 
-    public List<UserDTO> getAllUsers() {
-        return userRepository.findAll()
-                .stream()
-                .map(userMapper::toDto)
-                .toList();
+    public User getUserById(long id) {
+        return userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("User with this id does not exist."));
     }
 
-    public UserDTO getUserById(long id) {
-        User user = userRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException("User with id " + id + " not found!"));
-        return userMapper.toDto(user);
-    }
+    @Transactional
+    public User patchUser(Long id, @Valid UserPatchRO ro) {
+        User user = userRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("User with this id does not exist.")
+        );
 
-    public UserDTO updateUser(Long id, @Valid UserRO ro) {
-        User user = userRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException("User not found!"));
-
-        if (!user.getEmail().equals(ro.email()) && userRepository.existsByEmail(ro.email())) {
-            throw new IllegalArgumentException("Email already exists!");
+        if (ro.displayName().isPresent()) {
+            String displayName = ro.displayName().get();
+            if (userRepository.existsByDisplayName(displayName)) {
+                throw new IllegalArgumentException("User with this display name already exists.");
+            }
+            user.setDisplayName(displayName);
         }
 
-        if (!user.getDisplayName().equals(ro.displayName()) && userRepository.existsByDisplayName(ro.displayName())) {
-            throw new IllegalArgumentException("Username already exists!");
+        if (ro.email().isPresent()) {
+            String email = ro.email().get();
+            if (userRepository.existsByDisplayName(email)) {
+                throw new IllegalArgumentException("User with this email already exists.");
+            }
+            user.setEmail(email);
         }
 
-        /*userMapper.updateUserFromRO(ro, user);*/
+        ro.firstName().ifPresent(user::setFirstName);
+        ro.middleName().ifPresent(user::setMiddleName);
+        ro.lastName().ifPresent(user::setLastName);
+        ro.phoneNumber().ifPresent(user::setPhoneNumber);
+        ro.password().ifPresent(user::setPassword);
+        ro.birthDate().ifPresent(user::setBirthDate);
+        ro.description().ifPresent(user::setDescription);
 
-        if (ro.password() != null && !ro.password().isBlank()) {
-            user.setPassword(ro.password());
-        }
-
-        return userMapper.toDto(userRepository.save(user));
+        return userRepository.save(user);
     }
 
-    public void deleteUserById(long id) {
-        userRepository.deleteById(id);
+    @Transactional
+    public User deleteUser(Long id) {
+        User user = userRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("User with this id does not exist.")
+        );
+
+        userRepository.delete(user);
+        return user;
     }
 }
