eabb2e90d3dd953d92a39bb37b6002b2d0c0faf1
Dray <dray.lao@gmail.com>
2025-05-08 14:47:37 +0800

    DeveloperService done

--- src/main/java/com/firstgroup/gamemanagerapi/repository/DeveloperRepository.java
+++ src/main/java/com/firstgroup/gamemanagerapi/repository/DeveloperRepository.java
@@ -3,10 +3,12 @@ package com.firstgroup.gamemanagerapi.repository;
 import com.firstgroup.gamemanagerapi.entity.Developer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import java.util.Optional;
 @Repository
 public interface DeveloperRepository extends JpaRepository<Developer, Long> {
 
     boolean existsByEmail(String email);
     boolean existsByName(String name);
+
+    Optional<Developer> findByName(String name);
 }
