0dbf005c84a1ec33e10008db96c8653719223389
Dray <dray.lao@gmail.com>
2025-05-08 18:31:10 +0800

    dev

--- src/main/java/com/firstgroup/gamemanagerapi/service/DeveloperService.java
+++ src/main/java/com/firstgroup/gamemanagerapi/service/DeveloperService.java
@@ -16,7 +16,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@RequiredArgsConstructor()
 public class DeveloperService {
 
     private final DeveloperRepository developerRepository;
