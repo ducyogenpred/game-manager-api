45971a605b9d816850f793d2f216db890c3f6915
ducyogenpred <ducyogenpred@gmail.com>
2025-05-10 03:16:55 +0800

    Added isEmpty() for patchROs

--- src/main/java/com/firstgroup/gamemanagerapi/request/GamePatchRO.java
+++ src/main/java/com/firstgroup/gamemanagerapi/request/GamePatchRO.java
@@ -1,11 +1,17 @@
 package com.firstgroup.gamemanagerapi.request;
 
 import java.time.LocalDate;
+import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Stream;
 
 public record GamePatchRO(
         String title,
         LocalDate releaseDate,
         long publisherId,
         long developerId
-) {}
+) {
+    public boolean isEmpty() {
+        return Stream.of(title, releaseDate, publisherId, developerId).allMatch(Objects::isNull);
+    }
+}
--- src/main/java/com/firstgroup/gamemanagerapi/request/UserPatchRO.java
+++ src/main/java/com/firstgroup/gamemanagerapi/request/UserPatchRO.java
@@ -1,6 +1,8 @@
 package com.firstgroup.gamemanagerapi.request;
 
 import java.time.LocalDate;
+import java.util.Objects;
+import java.util.stream.Stream;
 
 public record UserPatchRO(
         String firstName,
@@ -12,4 +14,9 @@ public record UserPatchRO(
         String password,
         LocalDate birthDate,
         String description
-) {}
+) {
+    public boolean isEmpty() {
+        return Stream.of(firstName, middleName, lastName, displayName, email, phoneNumber, password, birthDate, description)
+                .allMatch(Objects::isNull);
+    }
+}
