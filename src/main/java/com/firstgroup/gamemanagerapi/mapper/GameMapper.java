d697ac13c5ab45ffe96300bfd80c03049467bd58
ducyogenpred <ducyogenpred@gmail.com>
2025-05-10 02:56:30 +0800

    Added GameMapper.java Improved Mapper

--- src/main/java/com/firstgroup/gamemanagerapi/mapper/GameMapper.java
+++ src/main/java/com/firstgroup/gamemanagerapi/mapper/GameMapper.java
@@ -2,11 +2,26 @@ package com.firstgroup.gamemanagerapi.mapper;
 
 import com.firstgroup.gamemanagerapi.dto.GameDTO;
 import com.firstgroup.gamemanagerapi.entity.Game;
+import com.firstgroup.gamemanagerapi.request.GamePatchRO;
 import com.firstgroup.gamemanagerapi.request.GameRO;
-import org.mapstruct.Mapper;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring")
 public interface GameMapper {
     GameDTO toDto(Game game);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "genres", ignore = true)
+    @Mapping(target = "users", ignore = true)
     Game toEntity(GameRO ro);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "genres", ignore = true)
+    @Mapping(target = "users", ignore = true)
+    void updateFromGamePatchRO(GamePatchRO ro, @MappingTarget Game entity);
 }
--- src/main/java/com/firstgroup/gamemanagerapi/mapper/UserMapper.java
+++ src/main/java/com/firstgroup/gamemanagerapi/mapper/UserMapper.java
@@ -4,22 +4,24 @@ import com.firstgroup.gamemanagerapi.dto.UserDTO;
 import com.firstgroup.gamemanagerapi.entity.User;
 import com.firstgroup.gamemanagerapi.request.UserPatchRO;
 import com.firstgroup.gamemanagerapi.request.UserRO;
-import org.mapstruct.BeanMapping;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValuePropertyMappingStrategy;
-
-import java.util.Optional;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     UserDTO toDto(User entity);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "reviews", ignore = true)
+    @Mapping(target = "games", ignore = true)
     User toEntity(UserRO ro);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-    void updateUserFromPatchRo(UserPatchRO ro, @MappingTarget User user);
-
-    default <T> T map(Optional<T> optional) {
-        return optional.orElse(null);
-    }
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "reviews", ignore = true)
+    @Mapping(target = "games", ignore = true)
+    void updateUserFromPatchRo(UserPatchRO ro, @MappingTarget User entity);
 }
