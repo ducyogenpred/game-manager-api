4190edc0ea661a5024afa28837293dc062dd2513
Dray <dray.lao@gmail.com>
2025-05-10 12:23:49 +0800

    dev

--- src/main/java/com/firstgroup/gamemanagerapi/controller/DeveloperController.java
+++ src/main/java/com/firstgroup/gamemanagerapi/controller/DeveloperController.java
@@ -3,6 +3,8 @@ package com.firstgroup.gamemanagerapi.controller;
 import com.firstgroup.gamemanagerapi.dto.DeveloperDTO;
 import com.firstgroup.gamemanagerapi.request.DeveloperRO;
 import com.firstgroup.gamemanagerapi.service.DeveloperService;
+import com.firstgroup.gamemanagerapi.entity.Developer;
+import com.firstgroup.gamemanagerapi.request.DeveloperPatchRO;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -19,7 +21,27 @@ public class DeveloperController {
 
     @PostMapping
     public ResponseEntity<DeveloperDTO> createDeveloper (@Valid @RequestBody DeveloperRO ro) {
-        return ResponseEntity.ok(developerService.createDeveloper(ro));
+        DeveloperDTO createdUser = developerService.createDeveloper(ro);
+        return ResponseEntity.ok(createdUser);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<DeveloperDTO> getDeveloperById (@PathVariable Long id) {
+        return ResponseEntity.ok(developerService.getDeveloperById(id));
+    }
+
+    @GetMapping("/name/{name}")
+    public ResponseEntity<DeveloperDTO> getDeveloperByName (@PathVariable String name) {
+        return ResponseEntity.ok(developerService.getDeveloperByName(name));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Developer> patchDeveloper (@PathVariable Long id, @Valid @RequestBody DeveloperPatchRO ro) {
+        return ResponseEntity.ok(developerService.patchDeveloper(id, ro));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Long> deleteDeveloper (@PathVariable Long id) {
+        return ResponseEntity.ok(developerService.deleteDeveloper(id));
+    }
 }
