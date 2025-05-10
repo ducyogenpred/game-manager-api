97cb2162f53c41c37f5b010a0277ce0eb3e37ca2
Dray <dray.lao@gmail.com>
2025-05-10 18:00:10 +0800

    DEbugged

---
+++ src/main/java/com/firstgroup/gamemanagerapi/util/ResponseUtils.java
@@ -0,0 +1,42 @@
+package com.firstgroup.gamemanagerapi.util;
+
+import com.firstgroup.gamemanagerapi.response.ErrorResponse;
+import com.firstgroup.gamemanagerapi.response.SuccessResponse;
+import org.springframework.http.HttpStatus;
+
+public class ResponseUtils {
+
+    public static <T> SuccessResponse<T> buildSuccessResponse(HttpStatus status, String message) {
+        SuccessResponse<T> response = new SuccessResponse<>();
+        response.setStatusCode(status.value());
+        response.setMessage(message);
+
+        return response;
+    }
+
+    public static <T> SuccessResponse<T> buildSuccessResponse(HttpStatus status, String message, T data) {
+        SuccessResponse<T> response = new SuccessResponse<>();
+        response.setStatusCode(status.value());
+        response.setMessage(message);
+        response.setData(data);
+
+        return response;
+    }
+
+    public static <T> ErrorResponse<T> buildErrorResponse(HttpStatus status, String message) {
+        ErrorResponse<T> response = new ErrorResponse<>();
+        response.setStatusCode(status.value());
+        response.setMessage(message);
+
+        return response;
+    }
+
+    public static <T> ErrorResponse<T> buildErrorResponse(HttpStatus status, String message, T data) {
+        ErrorResponse<T> response = new ErrorResponse<>();
+        response.setStatusCode(status.value());
+        response.setMessage(message);
+        response.setData(data);
+
+        return response;
+    }
+}
