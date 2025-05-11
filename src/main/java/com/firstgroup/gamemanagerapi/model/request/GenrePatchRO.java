edc816fad66d425b8cd2875b2fa46ba1a18e4ef9
ducyogenpred <ducyogenpred@gmail.com>
2025-05-12 01:59:34 +0800

    Updated RO

--- src/main/java/com/firstgroup/gamemanagerapi/model/request/GenrePatchRO.java
+++ src/main/java/com/firstgroup/gamemanagerapi/model/request/GenrePatchRO.java
@@ -1,16 +1,18 @@
 package com.firstgroup.gamemanagerapi.model.request;
 
-import org.apache.commons.lang3.StringUtils;
-
-import java.util.stream.Stream;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public record GenrePatchRO (
-    String name,
-    String email,
-    String description
+        @NotBlank(message = "Genre name must not be blank.")
+        String name,
+
+        @NotBlank(message = "Description must not be blank.")
+        @Pattern(regexp = "^\\S.*$", message = "Description must not start with a space.")
+        String description
 )  {
     public boolean isEmpty() {
-        return Stream.of(name,description).allMatch(StringUtils::isEmpty);
+        return name == null && description == null;
     }
 }
 
--- src/main/java/com/firstgroup/gamemanagerapi/model/request/GenreRO.java
+++ src/main/java/com/firstgroup/gamemanagerapi/model/request/GenreRO.java
@@ -1,8 +1,13 @@
 package com.firstgroup.gamemanagerapi.model.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public record GenreRO(
-        @NotBlank(message = "Genre name must not be blank.") String name,
-        @NotBlank(message = "Description must not be blank.") String description
+        @NotBlank(message = "Genre name must not be blank.")
+        String name,
+
+        @NotBlank(message = "Description must not be blank.")
+        @Pattern(regexp = "^\\S.*$", message = "Description must not start with a space.")
+        String description
 ) {}
