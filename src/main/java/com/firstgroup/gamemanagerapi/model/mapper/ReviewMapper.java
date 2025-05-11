ab49b9267fe0c3c9e5d26300f4e0068286d2a1bc
ducyogenpred <ducyogenpred@gmail.com>
2025-05-11 18:11:40 +0800

    Updated ReviewMapper.java

--- src/main/java/com/firstgroup/gamemanagerapi/model/mapper/ReviewMapper.java
+++ src/main/java/com/firstgroup/gamemanagerapi/model/mapper/ReviewMapper.java
@@ -2,11 +2,22 @@ package com.firstgroup.gamemanagerapi.model.mapper;
 
 import com.firstgroup.gamemanagerapi.model.dto.ReviewDTO;
 import com.firstgroup.gamemanagerapi.model.entity.Review;
+import com.firstgroup.gamemanagerapi.model.request.ReviewPatchRO;
 import com.firstgroup.gamemanagerapi.model.request.ReviewRO;
-import org.mapstruct.Mapper;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring")
 public interface ReviewMapper {
-    ReviewDTO toDto (Review review);
+    ReviewDTO toDto (Review entity);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
     Review toEntity (ReviewRO ro);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    void updateFromReviewPatchRO(ReviewPatchRO ro, @MappingTarget Review entity);
 }
