e38222fc9019ea788e53f8c0249bd3fb0c894d59
ducyogenpred <ducyogenpred@gmail.com>
2025-05-12 03:23:47 +0800

    Added GameGenre

--- src/main/java/com/firstgroup/gamemanagerapi/model/entity/GameGenre.java
+++ src/main/java/com/firstgroup/gamemanagerapi/model/entity/GameGenre.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Table(name = "games_genres")
 @Getter
@@ -16,6 +18,12 @@ public class GameGenre {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
     @ManyToOne
     @JoinColumn(name = "genre_id")
     @JsonBackReference
@@ -25,4 +33,15 @@ public class GameGenre {
     @JsonBackReference
     @JoinColumn(name = "game_id")
     private Game game;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
 }
