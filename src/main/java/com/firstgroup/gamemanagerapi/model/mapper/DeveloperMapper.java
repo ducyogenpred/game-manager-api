029a465e64fdb33ced938026dc2f67043d404c53
ducyogenpred <ducyogenpred@gmail.com>
2025-05-11 16:48:18 +0800

    Added mapping constraints

--- src/main/java/com/firstgroup/gamemanagerapi/model/mapper/DeveloperMapper.java
+++ src/main/java/com/firstgroup/gamemanagerapi/model/mapper/DeveloperMapper.java
@@ -4,12 +4,24 @@ import com.firstgroup.gamemanagerapi.model.dto.DeveloperDTO;
 import com.firstgroup.gamemanagerapi.model.entity.Developer;
 import com.firstgroup.gamemanagerapi.model.request.DeveloperRO;
 import com.firstgroup.gamemanagerapi.model.request.DeveloperPatchRO;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring")
 public interface DeveloperMapper {
     DeveloperDTO toDto(Developer developer);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "reviewCount", ignore = true)
+    @Mapping(target = "ratingAverage", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
     Developer toEntity(DeveloperRO developer);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "reviewCount", ignore = true)
+    @Mapping(target = "ratingAverage", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
     void updateDeveloperFromPatchRo(DeveloperPatchRO ro, @MappingTarget Developer entity);
 }
