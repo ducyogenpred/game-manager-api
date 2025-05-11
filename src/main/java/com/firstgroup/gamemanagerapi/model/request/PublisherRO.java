54d507b5fb460d2d163d0377648e9fde11e0090e
ducyogenpred <ducyogenpred@gmail.com>
2025-05-12 01:52:46 +0800

    Updated RO

--- src/main/java/com/firstgroup/gamemanagerapi/model/request/PublisherRO.java
+++ src/main/java/com/firstgroup/gamemanagerapi/model/request/PublisherRO.java
@@ -2,10 +2,17 @@ package com.firstgroup.gamemanagerapi.model.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public record PublisherRO(
-        @NotBlank(message = "Publisher name must not be blank") String name,
-        String Description,
-        @NotBlank(message = "Email must not be blank.") @Email String email
+        @NotBlank(message = "Publisher name must not be blank.")
+        String name,
+
+        @Pattern(regexp = "^\\S.*$", message = "Description must not start with a space.")
+        String description,
+
+        @NotBlank(message = "Email must not be blank.")
+        @Email(message = "Email must be a valid email address.")
+        String email
 ) {}
 
