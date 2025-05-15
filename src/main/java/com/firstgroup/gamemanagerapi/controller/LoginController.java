30580903f7e398d094d109e8698e0a37a2521819
Dray <dray.lao@gmail.com>
2025-05-15 15:38:11 +0800

    login

--- src/main/java/com/firstgroup/gamemanagerapi/controller/LoginController.java
+++ src/main/java/com/firstgroup/gamemanagerapi/controller/LoginController.java
@@ -6,7 +6,6 @@ import com.firstgroup.gamemanagerapi.model.entity.User;
 import com.firstgroup.gamemanagerapi.service.LoginService;
 import com.firstgroup.gamemanagerapi.util.MessageUtils;
 import com.firstgroup.gamemanagerapi.util.ResponseUtils;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +19,7 @@ public class LoginController {
     private final LoginService loginService;
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@Valid @RequestBody LoginRO ro) {
+    public ResponseEntity<?> login(@RequestBody LoginRO ro) {
         User user = loginService.login(ro.displayName(), ro.password());
 
         return ResponseEntity.ok(
